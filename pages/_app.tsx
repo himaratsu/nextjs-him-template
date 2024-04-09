f9c3bfd0aed25afe105c2ef6fc7f0e3d6eea2d84
himaratsu <himaratsu@gmail.com>
2024-04-09 20:31:41 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+import '@/styles/globals.css';
+
+import type { AppProps } from 'next/app';
+import { MantineProvider, createTheme } from '@mantine/core';
+import Head from 'next/head';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorSchemeScript } from '@mantine/core';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
+      <body className="bg-gray-50 leading-relaxed">
         <Main />
         <NextScript />
       </body>
